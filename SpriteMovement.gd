8b0090036bf5ec49bdefab4b4e0f051ed7b5afe1
yna2jd <123516015+yna2jd@users.noreply.github.com>
2024-04-12 20:01:39 -0400

    added speed

--- SpriteMovement.gd
+++ SpriteMovement.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@export var speed: float;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,8 +7,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("Move Right"):
 		velocity.x = 50;
 	if Input.is_action_pressed("Move Left"):
 		velocity.x = -50;
+	position += velocity * delta;
